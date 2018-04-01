4dde2ed8206fd90041a46df696934de3eea0fb35
whunt8 <31315907+whunt8@users.noreply.github.com>
2018-04-01 09:17:50 -0400

    Add Toolbar and supporting code

--- app/src/main/java/com/example/will/hw02/AppsFragment.java
+++ app/src/main/java/com/example/will/hw02/AppsFragment.java
@@ -9,14 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AppsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AppsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AppsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
--- app/src/main/java/com/example/will/hw02/MainActivity.java
+++ app/src/main/java/com/example/will/hw02/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.will.hw02;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AppsFragment.OnFragmentInteractionListener{
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_favorite) {
+
+            //todo: WRITE WHAT EACH MENU OPTION SHOULD DO
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    //interface for AppsFragment
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
